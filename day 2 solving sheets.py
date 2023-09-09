40caa286c3fa833eca27828d20fcc64d03e173b9
AnirudhChauhan7076 <144216732+AnirudhChauhan7076@users.noreply.github.com>
2023-09-09 21:45:40 +0530

    practice problems

---
+++ day 2 solving sheets.py
@@ -0,0 +1,25 @@
+'''print('a', end="")
+print('b', end="")
+print('c', end="")
+print('d', end="")
+
+
+s="welcome"
+print(type(s))
+
+x="\t\tWelcome\n"
+print(x.strip())
+
+y=eval("1+3*2")
+print(y)
+
+
+z=1
+z=z+2.5
+print(z)
+x,y=1,2
+x,y=y,x
+print(x,y)'''
+
+x, y=eval(input())
+print(x, y)
